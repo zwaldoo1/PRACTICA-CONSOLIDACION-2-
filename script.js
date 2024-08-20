304512ca4d93fd67ee9f045ee76aae9af394be22
Francisco Ojeda <112814902+zwaldoo1@users.noreply.github.com>
2024-08-19 20:24:14 -0400

    redessocialescambio

--- script.js
+++ script.js
@@ -18,7 +18,32 @@ document.getElementById('cv-download').addEventListener('click', function(event)
     var messageContainer = document.getElementById('message-container');
     messageContainer.textContent = 'Disponible próximamente';
     messageContainer.style.display = 'block';
-    
+ // Obtener el modal
+var modal = document.getElementById("myModal");
+
+// Obtener el botón que abre el modal
+var btn = document.getElementById("myBtn");
+
+// Obtener el elemento <span> que cierra el modal
+var span = document.getElementsByClassName("close")[0];
+
+// Cuando el usuario hace clic en el botón, se abre el modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// Cuando el usuario hace clic en <span> (x), se cierra el modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// Cuando el usuario hace clic fuera del modal, se cierra
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
     // hide the message after 3 seconds
     setTimeout(function() {
       messageContainer.style.display = 'none';
